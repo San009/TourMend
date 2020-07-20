0ea076cdfb91fede3fbff14149af6d23cbb73c5e
SANDIP <parajulisandip.sp@gmail.com>
2020-07-20 19:56:39 +0545

    Adding searchlist & Event UI

--- lib/screens/eventsPage.dart
+++ lib/screens/eventsPage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../widgets/mainPageWidgets/eventsWidgets/formWidget/Reportevent.dart';
+import '../widgets/eventsWidgets/formWidget/Reportevent.dart';
 
 /// This Widget is the main application widget.
 class EventPage extends StatelessWidget {
